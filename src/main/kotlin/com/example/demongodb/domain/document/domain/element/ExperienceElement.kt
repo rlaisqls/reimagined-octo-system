c8e8b1a68526bf960cbfbdc2951b2bbb39a95b19
김은빈 <rlaisqls@gmail.com>
2022-12-11 23:23:24 +0900

    Create: expElement

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/element/ExperienceElement.kt
@@ -0,0 +1,12 @@
+package com.example.demongodb.domain.document.domain.element
+
+import java.time.YearMonth
+
+class ExperienceElement (
+
+    val title: String,
+    val startDate: YearMonth,
+    val endDate: YearMonth,
+    val description: String
+
+)
