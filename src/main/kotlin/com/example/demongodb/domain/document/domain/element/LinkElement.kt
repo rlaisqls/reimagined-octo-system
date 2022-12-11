c2ec724b7a43fde0dcb6772669313212dc828de8
김은빈 <rlaisqls@gmail.com>
2022-12-11 23:22:18 +0900

    Create: LinkElement

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/element/LinkElement.kt
@@ -0,0 +1,10 @@
+package com.example.demongodb.domain.document.domain.element
+
+import com.example.demongodb.domain.document.domain.element.enums.LinkType
+
+class LinkElement (
+
+    val link: String,
+    val type: LinkType
+
+)
