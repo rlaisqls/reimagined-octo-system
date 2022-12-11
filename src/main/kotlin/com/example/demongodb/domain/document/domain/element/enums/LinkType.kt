3e84bb008fb7a6134485404b3031f1132c4f0d57
김은빈 <rlaisqls@gmail.com>
2022-12-11 23:21:53 +0900

    Create: LinkType

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/element/enums/LinkType.kt
@@ -0,0 +1,12 @@
+package com.example.demongodb.domain.document.domain.element.enums
+
+enum class LinkType {
+    LINK,
+    GITHUB,
+    NOTION,
+    TISTORY,
+    VELOG,
+    LINKEDIN,
+    INSTAGRAM,
+    FACEBOOK
+}
