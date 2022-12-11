baa868fef27035142a35e897719877ccbdd5f4de
김은빈 <rlaisqls@gmail.com>
2022-12-11 23:22:03 +0900

    Create: UserInfoElement

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/element/UserInfoElement.kt
@@ -0,0 +1,8 @@
+package com.example.demongodb.domain.document.domain.element
+
+class UserInfoElement (
+    val name: String,
+    val email: String,
+    val jobTitle: String = "backend",
+    val profileImageUrl: String = "https://kasdlkjasdlkasjdlaksjasd"
+)
