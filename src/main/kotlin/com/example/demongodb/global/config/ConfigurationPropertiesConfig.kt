d6704a9bf82f6d4397f95fe3c9681e4c2010cb0d
김은빈 <rlaisqls@gmail.com>
2022-12-11 23:18:36 +0900

    Create: ConfigurationMappingConfig

---
+++ src/main/kotlin/com/example/demongodb/global/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package com.example.demongodb.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan("com.example.demongodb")
+@Configuration
+class ConfigurationPropertiesConfig
