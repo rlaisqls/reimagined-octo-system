eaf3d7e362a7bbaf34901f606b709cc3a77077a9
김은빈 <rlaisqls@gmail.com>
2022-12-25 22:38:13 +0900

    update: Writer

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/element/WriterInfoElement.kt
@@ -0,0 +1,34 @@
+package com.example.demongodb.domain.document.domain.element
+
+import com.example.demongodb.domain.document.domain.element.enums.JobTitle
+import com.example.demongodb.domain.student.domain.Student
+import com.example.demongodb.domain.user.domain.User
+import java.util.UUID
+
+class WriterInfoElement (
+
+    val id: UUID,
+    val name: String,
+    val email: String,
+
+    val grade: Int,
+    val classNum: Int,
+    val number: Int,
+
+    val jobTitle: JobTitle
+
+) {
+    constructor(user: User, student: Student, jobTitle: JobTitle) : this(
+
+        id = user.id,
+        name = user.name,
+        email = user.email,
+
+        grade = student.grade,
+        classNum = student.classNum,
+        number = student.number,
+
+        jobTitle = jobTitle
+
+    )
+}
