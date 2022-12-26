4150a180a695f80b46aa806ff22d5efa3e019ac4
김은빈 <rlaisqls@gmail.com>
2022-12-26 20:43:37 +0900

    create: DocumentFacade

---
+++ src/main/kotlin/com/example/demongodb/domain/document/facade/DocumentFacade.kt
@@ -0,0 +1,24 @@
+package com.example.demongodb.domain.document.facade
+
+import com.example.demongodb.domain.document.domain.Document
+import com.example.demongodb.domain.document.domain.repository.DocumentRepository
+import com.example.demongodb.domain.document.error.DocumentNotFoundException
+import com.example.demongodb.domain.student.domain.Student
+import com.example.demongodb.domain.user.domain.User
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class DocumentFacade(
+    private val documentRepository: DocumentRepository
+) {
+
+    fun queryById(documentId: UUID): Document {
+        return documentRepository.queryById(documentId)?: throw DocumentNotFoundException
+    }
+
+    fun queryByStudent(user: User): List<Document> {
+        return documentRepository.queryByWriter_Id(user.id)
+    }
+
+}
