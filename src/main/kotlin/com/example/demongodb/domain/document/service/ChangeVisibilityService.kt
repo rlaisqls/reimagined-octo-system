aff13b775b1db52d850a5a3bc91b71fc57af8649
김은빈 <rlaisqls@gmail.com>
2022-12-26 20:47:11 +0900

    feat: ChangeVisibility

---
+++ src/main/kotlin/com/example/demongodb/domain/document/service/ChangeVisibilityService.kt
@@ -0,0 +1,29 @@
+package com.example.demongodb.domain.document.service
+
+import com.example.demongodb.domain.document.domain.repository.DocumentRepository
+import com.example.demongodb.domain.document.error.DocumentNotFoundException
+import com.example.demongodb.domain.document.facade.DocumentFacade
+import com.example.demongodb.domain.document.presentation.dto.request.ChangeVisibilityRequest
+import com.example.demongodb.domain.document.presentation.dto.request.CreateDocumentRequest
+import com.example.demongodb.domain.user.facade.UserFacade
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class ChangeVisibilityService (
+    private val userFacade: UserFacade,
+    private val documentFacade: DocumentFacade,
+    private val documentRepository: DocumentRepository
+) {
+    @Transactional
+    fun execute(request: ChangeVisibilityRequest) {
+
+        val user = userFacade.getCurrentUser()
+        val document = documentFacade.queryById(request.documentId)
+
+        if(!document.isWriter(user)) throw DocumentNotFoundException
+
+        document.changeVisibility(request.visibility)
+        documentRepository.save(document)
+    }
+}
