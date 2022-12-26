8263d42521e59681ae3ed26b7261df69fa2f5d8a
김은빈 <rlaisqls@gmail.com>
2022-12-26 20:42:33 +0900

    create: CustomDocumentRepository

---
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/repository/CustomDocumentRepository.kt
@@ -0,0 +1,18 @@
+package com.example.demongodb.domain.document.domain.repository
+
+import com.example.demongodb.domain.document.domain.Document
+import com.example.demongodb.domain.document.domain.element.WriterInfoElement
+import com.example.demongodb.domain.document.domain.element.enums.JobTitle
+import java.io.Writer
+
+interface CustomDocumentRepository {
+
+    /*
+    fun queryByVersionAndWriterInfo(
+        version: Int,
+        grade: Int?,
+        classNum: Int?,
+        jobTitle: JobTitle?
+    ): List<Document>
+*/
+}
