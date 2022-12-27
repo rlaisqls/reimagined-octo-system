98f03b0f856a4d674c9d39fd82c6184473cdc934
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:19:14 +0900

    update: 동적쿼리

--- src/main/kotlin/com/example/demongodb/domain/document/domain/repository/CustomDocumentRepositoryImpl.kt
+++ src/main/kotlin/com/example/demongodb/domain/document/domain/repository/CustomDocumentRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package com.example.demongodb.domain.document.domain.repository
 
-/*
+
+import com.example.demongodb.domain.document.domain.Document
+import com.example.demongodb.domain.document.domain.QDocument.document
+import com.example.demongodb.domain.document.domain.element.enums.JobTitle
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 
 class CustomDocumentRepositoryImpl(
@@ -14,15 +18,26 @@ class CustomDocumentRepositoryImpl(
         jobTitle: JobTitle?
     ): List<Document> {
 
-        /*
         return queryFactory
             .selectFrom(document)
             .where(
-                document.version.eq(version)
+                document.version.eq(version),
+                eqGrade(grade),
+                eqClassNum(classNum),
+                eqJobTitle(jobTitle)
             )
             .fetch()
-        */
-        return listOf()
     }
 
-}*/
+    private fun eqGrade(grade: Int?): BooleanExpression? {
+        return if(grade!=null) document.writer.grade.eq(grade) else null
+    }
+
+    private fun eqClassNum(classNum: Int?): BooleanExpression? {
+        return if(classNum!=null) document.writer.classNum.eq(classNum) else null
+    }
+
+    private fun eqJobTitle(jobTitle: JobTitle?): BooleanExpression? {
+        return if(jobTitle!=null) document.writer.jobTitle.eq(jobTitle) else null
+    }
+}
