03212975c6e594456e3e96d3458380527c7ad8f1
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:23:01 +0900

    update: QueryExclude

--- src/main/kotlin/com/example/demongodb/global/entity/BaseUUIDEntity.kt
+++ src/main/kotlin/com/example/demongodb/global/entity/BaseUUIDEntity.kt
@@ -1,13 +1,14 @@
 package com.example.demongodb.global.entity
 
-
+import com.querydsl.core.annotations.QueryExclude
 import jakarta.persistence.Column
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.GenericGenerator
-import org.springframework.data.annotation.Id
 import java.util.*
 
+@QueryExclude
 @MappedSuperclass
 class BaseUUIDEntity {
 
