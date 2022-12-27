9023b253ae7bf0f7749aba7035a16de117517d6f
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:21:09 +0900

    update: properitesBinding

--- src/main/kotlin/com/example/demongodb/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/com/example/demongodb/global/security/jwt/JwtProperties.kt
@@ -1,15 +1,11 @@
 package com.example.demongodb.global.security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Configuration
-import java.util.*
 
-//@ConstructorBinding
-@Configuration
+
 @ConfigurationProperties(prefix = "jwt")
-data class JwtProperties(
-    val secretKey: String = Base64.getEncoder().encodeToString("c2lsdmVybmluZS10ZWNoLXNwcmluZy1ib290LWp3dC10dXRvcmlhbC1zZWNyZXQtc2lsdmVybmluZS10ZWNoLXNwcmluZy1ib290LWp3dC10dXRvcmlhbC1zZWNyZXQK".toByteArray()),
-    val accessExp: Long = 180000,
-    val refreshExp: Long = 180 //TODO DLRJ
+class JwtProperties(
+    val secretKey: String,
+    val accessExp: Long,
+    val refreshExp: Long
 )
