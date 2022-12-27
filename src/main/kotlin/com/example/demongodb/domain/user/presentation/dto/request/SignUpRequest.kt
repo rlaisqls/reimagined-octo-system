8244379773660b613f379cf7e8161aa86b676bb9
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:53:43 +0900

    update: Student 정보 입력

--- src/main/kotlin/com/example/demongodb/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/demongodb/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -1,23 +1,30 @@
 package com.example.demongodb.domain.user.presentation.dto.request
 
 import com.example.demongodb.global.util.RegexpUtil
-import org.hibernate.validator.constraints.Length
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
+import org.hibernate.validator.constraints.Length
+import org.hibernate.validator.constraints.Range
 
 data class SignUpRequest(
 
-    @field:Length
+    @field:Length(max = 15)
     @field:NotBlank
     val name: String,
 
+    @field:Length(max = 60)
     @field:Email
     @field:NotBlank
     val email: String,
 
     @field:Pattern(regexp = RegexpUtil.PASSWORD_PATTERN)
     @field:NotBlank
-    val password: String
+    val password: String,
+
+    @field:Range(min=1101, max=3320)
+    @field:NotNull
+    val studentID: Int
 
 )
