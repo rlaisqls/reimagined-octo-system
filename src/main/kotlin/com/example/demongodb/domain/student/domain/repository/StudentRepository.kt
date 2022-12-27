ec1696a28888e703adc6b7a45370c58529207f9d
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:51:15 +0900

    bug: User is nullable

--- src/main/kotlin/com/example/demongodb/domain/student/domain/repository/StudentRepository.kt
+++ src/main/kotlin/com/example/demongodb/domain/student/domain/repository/StudentRepository.kt
@@ -1,11 +1,10 @@
 package com.example.demongodb.domain.student.domain.repository
 
-import com.example.demongodb.domain.document.domain.Document
 import com.example.demongodb.domain.student.domain.Student
 import com.example.demongodb.domain.user.domain.User
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.*
 
 interface StudentRepository: MongoRepository<Student, UUID> {
-    fun findByUser(user: User): Student
+    fun findByUser(user: User): Student?
 }
