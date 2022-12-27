879c218d38c6856902010d5980ac399abf1fcfc9
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:48:34 +0900

    update: DocumentID 추가

--- src/main/kotlin/com/example/demongodb/domain/document/presentation/dto/request/ChangeVisibilityRequest.kt
+++ src/main/kotlin/com/example/demongodb/domain/document/presentation/dto/request/ChangeVisibilityRequest.kt
@@ -1,7 +1,9 @@
 package com.example.demongodb.domain.document.presentation.dto.request
 
 import com.example.demongodb.domain.document.domain.enums.Visibility
+import java.util.UUID
 
 data class ChangeVisibilityRequest (
+    val documentId: UUID,
     val visibility: Visibility
 )
