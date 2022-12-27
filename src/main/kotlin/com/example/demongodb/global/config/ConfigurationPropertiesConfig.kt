7ddfa896fe1babac71a381e4dbfd7cd6195ed089
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:47:54 +0900

    update: @EnableConfigurationProperties

--- src/main/kotlin/com/example/demongodb/global/config/ConfigurationPropertiesConfig.kt
+++ src/main/kotlin/com/example/demongodb/global/config/ConfigurationPropertiesConfig.kt
@@ -1,8 +1,10 @@
 package com.example.demongodb.global.config
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
+@EnableConfigurationProperties
 @ConfigurationPropertiesScan("com.example.demongodb")
 @Configuration
 class ConfigurationPropertiesConfig
