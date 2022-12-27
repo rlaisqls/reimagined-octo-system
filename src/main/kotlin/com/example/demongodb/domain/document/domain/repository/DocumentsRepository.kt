889e030e352a284143a97871f460a302efc84187
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:45:04 +0900

    delete: 임시파일 삭제

--- src/main/kotlin/com/example/demongodb/domain/document/domain/repository/DocumentsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demongodb.domain.document.domain.repository
-
-import com.example.demongodb.domain.document.domain.Document
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface DocumentsRepository: MongoRepository<Document, ObjectId> {
-}
