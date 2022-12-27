6acd2a9b1278ca75be5e1f03275b57f8d0efe2fc
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:49:58 +0900

    update: documentId 파라미터로 받음

--- src/main/kotlin/com/example/demongodb/domain/document/presentation/dto/request/CreateDocumentRequest.kt
+++ src/main/kotlin/com/example/demongodb/domain/document/presentation/dto/request/CreateDocumentRequest.kt
@@ -1,5 +1,8 @@
 package com.example.demongodb.domain.document.presentation.dto.request
 
+import com.example.demongodb.domain.document.domain.element.enums.JobTitle
+
 data class CreateDocumentRequest (
-    val version: Int
+    val version: Int,
+    val jobTitle: JobTitle
 )
--- src/main/kotlin/com/example/demongodb/domain/document/service/QueryDocumentInfoService.kt
+++ src/main/kotlin/com/example/demongodb/domain/document/service/QueryDocumentInfoService.kt
@@ -3,13 +3,12 @@ package com.example.demongodb.domain.document.service
 import com.example.demongodb.domain.document.domain.enums.Visibility
 import com.example.demongodb.domain.document.error.DocumentAccessRightException
 import com.example.demongodb.domain.document.facade.DocumentFacade
-import com.example.demongodb.domain.document.presentation.dto.request.QueryDocumentInfoRequest
 import com.example.demongodb.domain.document.presentation.dto.response.QueryDocumentInfoResponse
-import com.example.demongodb.domain.document.presentation.dto.response.QueryDocumentListResponse
 import com.example.demongodb.domain.user.domain.enums.Authority
 import com.example.demongodb.domain.user.facade.UserFacade
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class QueryDocumentInfoService(
@@ -17,13 +16,13 @@ class QueryDocumentInfoService(
     private val documentFacade: DocumentFacade
 ) {
     @Transactional
-    fun execute(request: QueryDocumentInfoRequest): QueryDocumentInfoResponse {
+    fun execute(documentId: UUID): QueryDocumentInfoResponse {
 
         val user = userFacade.getCurrentUser()
-        val document = documentFacade.queryById(request.documentId)
+        val document = documentFacade.queryById(documentId)
 
         if((document.visibility == Visibility.PRIVATE) ||
-            (user.authority == Authority.USER && !document.isWriter(user))) {
+            (user.authority == Authority.STUDENT && !document.isWriter(user))) {
 
             throw DocumentAccessRightException
         }
