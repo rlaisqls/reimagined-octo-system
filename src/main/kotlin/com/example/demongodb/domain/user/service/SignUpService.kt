b30a68c085219d55c4fa6c1c450e20736523dcd5
김은빈 <rlaisqls@gmail.com>
2022-12-27 13:52:26 +0900

    update: Student 저장

--- src/main/kotlin/com/example/demongodb/domain/user/service/SignUpService.kt
+++ src/main/kotlin/com/example/demongodb/domain/user/service/SignUpService.kt
@@ -1,22 +1,25 @@
 package com.example.demongodb.domain.user.service
 
 import com.example.demongodb.domain.auth.presentation.dto.response.TokenResponse
+import com.example.demongodb.domain.student.domain.Student
+import com.example.demongodb.domain.student.domain.repository.StudentRepository
 import com.example.demongodb.domain.user.domain.User
 import com.example.demongodb.domain.user.domain.enums.Authority
 import com.example.demongodb.domain.user.domain.repository.UserRepository
 import com.example.demongodb.domain.user.facade.UserFacade
 import com.example.demongodb.domain.user.presentation.dto.request.SignUpRequest
 import com.example.demongodb.global.security.jwt.JwtTokenProvider
+import jakarta.transaction.Transactional
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
-import jakarta.transaction.Transactional
 
 @Service
 class SignUpService(
     private val passwordEncoder: PasswordEncoder,
     private val userRepository: UserRepository,
     private val userFacade: UserFacade,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val studentRepository: StudentRepository
 ) {
     @Transactional
     fun execute(request: SignUpRequest): TokenResponse {
@@ -28,7 +31,14 @@ class SignUpService(
                 name = request.name,
                 password = passwordEncoder.encode(request.password),
                 email = request.email,
-                authority = Authority.USER
+                authority = Authority.STUDENT
+            )
+        )
+
+        studentRepository.save(
+            Student(
+                user = user,
+                studentID = request.studentID
             )
         )
 
