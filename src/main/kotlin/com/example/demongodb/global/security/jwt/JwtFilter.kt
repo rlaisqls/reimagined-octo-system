2a7bdb37bc288ae2ab20d88294492bd06e830dc1
rlaisqls <rlaisqls@gmail.com>
2023-01-14 10:13:00 +0900

    update: 디버깅 코드 삭제

--- src/main/kotlin/com/example/demongodb/global/security/jwt/JwtFilter.kt
+++ src/main/kotlin/com/example/demongodb/global/security/jwt/JwtFilter.kt
@@ -1,12 +1,12 @@
 package com.example.demongodb.global.security.jwt
 
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
 
 
 class JwtFilter(
@@ -17,8 +17,6 @@ class JwtFilter(
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
 
         val token = jwtTokenProvider.resolveToken(request)
-        println("JwtFilter.doFilterInternal")
-        println(token)
         if (token != null) {
             val authentication = jwtTokenProvider.getAuthentication(token)
             SecurityContextHolder.getContext().authentication = authentication
